5060b056f217a65d13d58d979bdbcbf623d6d647
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-02-24 21:13:15 -0800

    moving 'yelo' to 'service'

--- service/settings.py
+++ service/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for yelo project.
+Django settings for service project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'yelo.urls'
+ROOT_URLCONF = 'service.urls'
 
-WSGI_APPLICATION = 'yelo.wsgi.application'
+WSGI_APPLICATION = 'service.wsgi.application'
 
 
 # Database
--- service/wsgi.py
+++ service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yelo project.
+WSGI config for service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yelo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
