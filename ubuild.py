b6c179676d4764400fc5f1a60e53536e49f185ab
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2016-01-13 17:10:24 -0800

    fixing up uranium script.

--- ubuild.py
+++ ubuild.py
@@ -1,3 +1,6 @@
+import uranium
+import subprocess
+
 REQUIREMENTS = {
     "django": "==1.9.1",
     "djangorestframework": "==3.3.2",
@@ -7,6 +10,25 @@ REQUIREMENTS = {
 }
 
 
+@build.task
 def main(build):
     for pkg, version in REQUIREMENTS.items():
         build.packages.install(pkg, version=version)
+    return setup_db(build)
+
+
+def setup_db(build):
+    return subprocess.call(["./manage.py", "migrate"])
+
+
+@uranium.task_requires(main)
+def develop(build):
+    return subprocess.call(
+        ["./manage.py", "runserver", "0.0.0.0:9001"]
+    )
+
+
+@uranium.task_requires(main)
+def test(build):
+    build.packages.install("py.test")
+    return subprocess.call(["py.test", "tests"])
